c3b400c0e081afa213d46e3dea31729ebc38e803
Edward D <edward@wpartisan.me>
2017-03-20 18:52:33 +0000

    Minify css & remove jquery migrate

--- functions.php
+++ functions.php
@@ -13,14 +13,23 @@ function wpna_styles() {
 	wp_enqueue_style( 'wpna-icons', get_template_directory_uri() . '/assets/css/icons.min.css', null, '1.0.0', 'all' );
 	wp_enqueue_style( 'wpna-bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', null, '1.0.0', 'all' );
 	// wp_enqueue_style( 'wpna-flexslider', get_template_directory_uri() . '/assets/css/flexslider.min.css', null, '1.0.0', 'all' );
-	wp_enqueue_style( 'wpna-fairsky', get_template_directory_uri() . '/assets/css/theme-fairsky.css', null, '1.0.0', 'all' );
+	wp_enqueue_style( 'wpna-fairsky', get_template_directory_uri() . '/assets/css/theme-fairsky.min.css', null, '1.0.0', 'all' );
 	wp_enqueue_style( 'wpna-custom', get_template_directory_uri() . '/assets/css/custom.css', null, '1.0.0', 'all' );
 	wp_enqueue_style( 'wpna-gfonts', 'https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic', null, '1.0.1', 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'wpna_styles' );
 
+add_filter( 'wp_default_scripts', 'dequeue_jquery_migrate' );
+function dequeue_jquery_migrate( &$scripts){
+	if(!is_admin()){
+		$scripts->remove( 'jquery');
+		$scripts->add( 'jquery', false, array( 'jquery-core' ), '1.10.2' );
+	}
+}
+
 function wpna_scripts() {
-	wp_enqueue_script( 'jquery' );
+	wp_deregister_script( 'jquery' ); // remove standard jquery
+	wp_enqueue_script( 'jquery', get_template_directory_uri() .'/assets/js/jquery.min.js', array(), '1.11.1', true);
 	// wp_enqueue_script( 'wpna-tweenmax', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js', array(), '1.0.1', true );
 	// wp_enqueue_script( 'wpna-ScrollToPlugin', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/ScrollToPlugin.min.js', array(), '1.0.1', true );
 	wp_enqueue_script( 'wpna-bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(), '1.0.1', true );
